cc6777871132bcc3809aded768335269ef1d50fd
Artem Kondyukov <artemkondyukov@gmail.com>
2021-03-08 20:10:35 +0300

    docker in AWS, not GH

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 yum -y update
-yum -y install python3 python3-pip
-pip-3 install -r /opt/feedback-bot/requirements.txt
+yum -y install docker-ce docker-ce-cli containerd.io
+#yum -y install python3 python3-pip
+#pip-3 install -r /opt/feedback-bot/requirements.txt
+cd /opt/feedback-bot
+docker image build . -t feedback-bot-image
+docker run feedback-bot-image
