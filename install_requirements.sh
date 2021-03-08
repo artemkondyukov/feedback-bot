88e93bdf8f17255db44ca4a1df99ccfa18060798
Artem Kondyukov <artemkondyukov@gmail.com>
2021-03-08 20:33:38 +0300

    Fix docker installation

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 yum -y update
-yum -y install docker-ce docker-ce-cli containerd.io
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y docker-ce docker-ce-cli containerd.io
 #yum -y install python3 python3-pip
 #pip-3 install -r /opt/feedback-bot/requirements.txt
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
 cd /opt/feedback-bot
 docker image build . -t feedback-bot-image
 docker run feedback-bot-image
