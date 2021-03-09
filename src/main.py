e3163b511b6a315089cd6b5d87c95f59d3ad4340
Artem Kondyukov <artemkondyukov@gmail.com>
2021-03-09 10:57:19 +0300

    reply on /start with user info

--- src/main.py
+++ src/main.py
@@ -2,8 +2,30 @@ from telegram.ext import CommandHandler, Updater
 import os
 
 
+class User:
+    def __init__(self, username, first_name, last_name, chat_id):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.chat_id = chat_id
+
+    def __repr__(self):
+        return f"Username: {self.username}\n" \
+               f"Fist name: {self.first_name}\n" \
+               f"Last name: {self.last_name}\n" \
+               f"Chat id: {self.chat_id}"
+
+
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=User(
+            update.message.chat.username,
+            update.message.chat.first_name,
+            update.message.chat.last_name,
+            update.message.chat_id
+        )
+    )
 
 
 token = open("token", "r").read().strip()
